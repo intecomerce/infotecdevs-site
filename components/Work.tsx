79fa1d27cca3292fd626f6f9ca35f62917e6dc5d
Steve Silva <Steve.Silva@copart.com>
2023-05-29 12:31:10 -0300

    add 'project' component

--- components/Work.tsx
+++ components/Work.tsx
@@ -5,6 +5,7 @@ import work2 from "../public/images/work2.png";
 import work3 from "../public/images/work3.png";
 import work4 from "../public/images/work4.png";
 import work5 from "../public/images/work1.png";
+import Project from "./Project";
 
 const Work = () => {
   return (
@@ -12,45 +13,14 @@ const Work = () => {
       <Heading title="Projetos Criativos" />
 
       <div className="grid grid-cols-3 gap-4">
-        <div className="row-span-1 relative group">
-          <Image src={work1} alt="image" />
-          <div className="bg-[#000000bd] absolute w-[100%] top-0 opacity-0 transition duration-500 group-hover:opacity-100 grid place_items-center text-white">
-            Titulo do projeto
-          </div>
-        </div>
 
-        <div className="relative group">
-          <Image src={work2} alt="image" />
-          <div className="bg-[#000000bd] absolute w-[100%] top-0 opacity-0 transition duration-500 group-hover:opacity-100 grid place_items-center text-white">
-            Titulo do projeto
-          </div>
-        </div>
-        <div className="relative group">
-          <Image src={work3} alt="image" />
-          <div className="bg-[#000000bd] absolute w-[100%] top-0 opacity-0 transition duration-500 group-hover:opacity-100 grid place_items-center text-white">
-            Titulo do projeto
-          </div>
-        </div>
-
-        <div className="relative group">
-          <Image src={work4} alt="image" />
-          <div className="bg-[#000000bd] absolute w-[100%] top-0 opacity-0 transition duration-500 group-hover:opacity-100 grid place_items-center text-white">
-            Titulo do projeto
-          </div>
-        </div>
-
-        <div className="relative group">
-          <Image src={work5} alt="image" />
-          <div className="bg-[#000000bd] absolute w-[100%] top-0 opacity-0 transition duration-500 group-hover:opacity-100 grid place_items-center text-white">
-            Titulo do projeto
-          </div>
-        </div>
-        <div className="relative group">
-          <Image src={work2} alt="image" />
-          <div className="bg-[#000000bd] absolute w-[100%] top-0 opacity-0 transition duration-500 group-hover:opacity-100 grid place_items-center text-white">
-            Titulo do projeto
-          </div>
-        </div>
+        <Project title="Login" src={work1} onClick={()=>console.log("click")}/>
+        <Project title="Login" src={work2} onClick={()=>console.log("click")}/>
+        <Project title="Login" src={work3} onClick={()=>console.log("click")}/>
+        <Project title="Login" src={work4} onClick={()=>console.log("click")}/>
+        <Project title="Login" src={work5} onClick={()=>console.log("click")}/>
+        <Project title="Login" src={work2} onClick={()=>console.log("click")}/>
+        
       </div>
     </section>
   );
